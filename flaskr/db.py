16b33ea681d32254b1532b0700f5c2b7d4400eee
Skribinto <skribinto@gmail.com>
2019-04-07 21:26:53 +0200

    up to github

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,6 +6,7 @@ from flask.cli import with_appcontext
 
 
 def get_db():
+    '''Returns current db and creates one if none exist'''
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
@@ -21,3 +22,26 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    '''Inintialise fresh db'''
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
